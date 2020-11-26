cd91a621e3dba83289fc2074aaba221062170a58
f-ttok <f-tatsuoka@na.nuap.nagoya-u.ac.jp>
2020-11-26 10:43:59 +0900

    draft

--- test4.jl
+++ test4.jl
@@ -3,83 +3,45 @@ using DelimitedFiles
 using Printf
 
 cd(@__DIR__)
+include("./algorithms/de.jl")
+include("./algorithms/newton.jl")
 
 function generate_exact_solutions()
-    for matname in 
-end
-
-function compute_refsol()
-    println("compute reference solutions")
-    matnamelist = ["SPD_well", "SPD_ill", "NS_well", "NS_ill"]
-    for matname in matnamelist
-        println("$(matname)")
-        A_f64 = readdlm("matrices/test3_A_$(matname).txt")
-        A = convert(Array{BigFloat, 2}, A_f64)
+    for matname in ["SPD_well", "SPD_ill", "NS_well", "NS_ill"]
+        println("--- $(matname) ---")
+        A_f64 = collect(mmread("matrix/$(matname).mtx"))
+        writedlm("matrix/test4_A_$(matname).txt", A_f64)
+        A = convert(Array{BigFloat,2}, A_f64)
         n = size(A,1)
-        norm_A = norm(A)
-        B = (A / norm_A)
-
-        maxiter = 100
-        X = diagm(0 => ones(BigFloat, n))
-        N = copy(B)
-        relres = norm(X^2 - B)
-        maxiter = 100
-        iter = 0
-        tol = 1e-50
-
-        println("Square root")
-        while iter < maxiter && relres > tol
-            @printf("%4d | %.3e\n", iter, relres)
-            iter += 1
-            T = (I+N)/2
-            X = X*T
-            N = (T^2) \ N
-            relres = norm(X^2 - B)
-        end
+        tol = sqrt(n) * eps(BigFloat)/2
+        println("Computing A^0.2")
+        Exact2 = prootm_newton(A, 5, tol=tol)
+        println("Computing A^0.5")
+        Exact5 = sqrtm_newton(A, tol=tol)
+        Exact8 = Exact2^4
+        writedlm("matrix/test4_Exact2_$(matname).txt", convert(Array{Float64,2}, Exact2))
+        writedlm("matrix/test4_Exact5_$(matname).txt", convert(Array{Float64,2}, Exact5))
+        writedlm("matrix/test4_Exact8_$(matname).txt", convert(Array{Float64,2}, Exact8))
         println()
-
-        C = copy(X)
-        X = diagm(0 => ones(BigFloat, n))
-        N = copy(C)
-        norm_C = norm(C)
-        relres = norm(X^5 - C) / norm_C
-        maxiter = 100
-        iter = 0
-        tol = 1e-50
-        println("Fifth root")
-        while iter < maxiter && relres > tol
-            @printf("%4d | %.3e\n", iter, relres)
-            iter += 1
-            T = (4I+N)/5
-            X = X*T
-            N = (T^5) \ N
-            relres = norm(X^5 - C) / norm_C
-        end
-
-        Exact = X * norm_A^(1/10)
-        Y = copy(Exact)
-        for i = 2:8
-            Y = Y * Exact
-            if i in [2,5,8]
-                writedlm("matrices/test3_Exact_$(matname)_$(i).txt", convert(Array{Float64,2}, Y))
-            end
-        end
     end
 end
 
 
 function main()
+    m_list = 10:10:200
+    Data = Array{Any,2}(undef, length(m_list)+1, 37)
+    Data
     for αx10 in [2, 5, 8]
         matnamelist = ["SPD_well", "SPD_ill", "NS_well", "NS_ill"]
         α = αx10/10
         for matname in matnamelist
             println("--- $(matname) ---")
-            A = readdlm("matrices/test3_A_$(matname).txt")
-            Ref = readdlm("matrices/test3_Exact_$(matname)_$(αx10).txt")
+            A = readdlm("matrix/test4_A_$(matname).txt")
+            Ref = readdlm("matrix/test4_Exact$(matname)_$(αx10).txt")
             norm_Ref = norm(Ref)
             ρ = maximum(abs.(eigvals(A)))
 
-            l, r = get_lr(A, α, 2.0^(-53)*ρ^α, use_arpack=false)
+            l, r = get_interval
 
             mlist = 10:10:200
             Errlist = zeros(length(mlist), 4)
@@ -95,7 +57,7 @@ function main()
                 X = powm_de(A, α, m, l, r)
                 Errlist[i,4] = norm(X - Ref) / norm_Ref
             end
-            writedlm("results/test3_$(matname)_$(αx10).txt", Errlist)
+            writedlm("result/test4_$(matname)_$(αx10).txt", Errlist)
             println()
         end
     end
