923fb02b14a445d72fd3c009f4e6e63f6a8ac8a5
Fuminori TATSUOKA <44796291+f-ttok@users.noreply.github.com>
2020-12-02 21:16:39 +0900

    add println("Test n")

--- run_alltests.jl
+++ run_alltests.jl
@@ -1,26 +1,31 @@
 
 cd(@__DIR__)
 
+println("Test 1")
 include("test1.jl")
 generate_exact_solutions()
 main()
 
 
+println("\n\nTest 2")
 include("test2.jl")
 generate_exact_solutions()
 main()
 
 
+println("\n\nTest 3")
 include("test3.jl")
 generate_exact_solutions()
 main()
 
 
+println("\n\nTest 4")
 include("test4.jl")
 generate_exact_solutions()
 main()
 
 
+println("\n\nTest 5")
 include("test5.jl")
 test5_spd()
 test5_general()
